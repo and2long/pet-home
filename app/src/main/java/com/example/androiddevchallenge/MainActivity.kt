156facff89016538a6991b77e04a0aeccf79645f
and2long <and2long@gmail.com>
2021-03-01 22:38:17 +0800

    显示宠物图片列表。

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,11 +18,23 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.model.PetBean
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -37,12 +49,63 @@ class MainActivity : AppCompatActivity() {
 }
 
 // Start building your app here!
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
-    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "宠物之家") }
+            )
+        },
+        content = {
+
+            val data = mutableListOf(
+                PetBean(0, "可爱小狗"),
+                PetBean(0, "可爱小狗"),
+                PetBean(0, "可爱小狗"),
+                PetBean(0, "可爱小狗"),
+                PetBean(0, "可爱小狗"),
+                PetBean(0, "可爱小狗"),
+                PetBean(0, "可爱小狗"),
+            )
+            LazyVerticalGrid(
+                cells = GridCells.Adaptive(minSize = 150.dp),
+                modifier = Modifier.padding(start = 5.dp, top = 5.dp, end = 5.dp, bottom = 5.dp)
+            ) {
+                items(data.size) { index ->
+                    Card(
+                        elevation = 3.dp,
+                        modifier = Modifier.padding(
+                            start = 5.dp,
+                            top = 5.dp,
+                            end = 5.dp,
+                            bottom = 10.dp
+                        )
+                    ) {
+                        Column {
+                            Image(
+                                painter = painterResource(R.mipmap.dog1),
+                                contentDescription = "",
+                                 modifier = Modifier.fillMaxWidth()
+                            )
+                            Text(
+                                text = data[index].title,
+                                Modifier.padding(
+                                    start = 10.dp,
+                                    top = 5.dp,
+                                    end = 5.dp,
+                                    bottom = 10.dp
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    )
 }
+
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
---
+++ app/src/main/java/com/example/androiddevchallenge/model/PetBean.kt
@@ -0,0 +1,3 @@
+package com.example.androiddevchallenge.model
+
+data class PetBean(val rid: Int, val title: String)
